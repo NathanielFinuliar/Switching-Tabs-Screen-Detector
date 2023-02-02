6128a1c4910dc31cdd77c02880305e38ae55b49e
Nathaniel Finuliar <104445164+NathanielFinuliar@users.noreply.github.com>
2023-02-03 05:26:20 +0800

    added capture function and import pyautogui

--- detector.py
+++ detector.py
@@ -1,5 +1,11 @@
+import pyautogui
+
 class Detector():
     def __init__(self, path, url):
         self.path = path
         self.webhook_url = url
+
+    def capture(self):
+        image = pyautogui.screenshot()
+        image.save(str(self.path))
 
